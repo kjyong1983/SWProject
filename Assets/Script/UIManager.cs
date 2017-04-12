814565f193546a87cefcb21a9e02cb7e7c1583a0
kjyong1983 <kjyong1983@gmail.com>
2017-04-12 20:24:28 +0900

    UI 에서 신 이동 구현중

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnStart()
+    {
+        SceneManager.LoadScene(1);
+        Debug.Log("Go to game Screen");
+    }
+
+    public void OpenInGameMenu()
+    {
+        //RectTransform GameMenu = 
+        Debug.Log("Open InGame Menu");
+    }
+
+}
