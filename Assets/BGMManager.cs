8475861419c1bb11ecf958dd4193645d2236fc17
kjyong1983 <kjyong1983@gmail.com>
2017-04-23 20:00:10 +0900

    BGM 켜고 끄기 가능.

---
+++ Assets/BGMManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour {
+
+    public AudioSource bgmSource;
+
+	// Use this for initialization
+	void Start () {
+        bgmSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (bgmSource.mute)
+            {
+                bgmSource.mute = false;
+            }
+            else
+            {
+                bgmSource.mute = true;
+            }
+            Debug.Log("Mute");
+
+        }
+
+	}
+}
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -8,15 +8,21 @@ public class UIManager : MonoBehaviour {
 
     GameObject gameMenu;
     GameObject titleMenuDialogue;
+    GameObject settingDialogue;
+
+    GameObject bgmManager;
 
 	// Use this for initialization
 	void Start () {
         gameMenu = GameObject.Find("GameMenu");
         titleMenuDialogue = GameObject.Find("TitleDialogue");
+        settingDialogue = GameObject.Find("settingDialogue");
+
+        bgmManager = GameObject.Find("BGMManager");
 
         gameMenu.SetActive(false);
         titleMenuDialogue.SetActive(false);
-
+        settingDialogue.SetActive(false);
 
 	}
 	
@@ -58,5 +64,39 @@ public class UIManager : MonoBehaviour {
         titleMenuDialogue.SetActive(false);
 
     }
+
+    public void OpenSettingDialogue()
+    {
+        settingDialogue.SetActive(true);
+    }
+
+    public void CloseSettingDialogue()
+    {
+        settingDialogue.SetActive(false);
+    }
+
+    public void BGMMute(Button button)
+    {
+        var bgm = bgmManager.GetComponent<BGMManager>();
+        //var buttonText = GameObject.Find("BGM").GetComponentInChildren<Text>();
+        var buttonText = button.GetComponentInChildren<Text>();
+
+        Debug.Log(buttonText);
+
+        if (!bgm.bgmSource.mute)
+        {
+            bgm.bgmSource.mute = true;
+            buttonText.text = "BGM Off";
+
+        }
+        else
+        {
+            bgm.bgmSource.mute = false;
+            buttonText.text = "BGM On";
+
+        }
+
+    }
+
 
 }
