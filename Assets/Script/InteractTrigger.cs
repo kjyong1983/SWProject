92f6d72315310aea20ffb427ffc42d2085c18a11
kjyong1983 <kjyong1983@gmail.com>
2017-04-26 18:44:13 +0900

    아이템 상호작용 스크립트.

---
+++ Assets/Script/InteractTrigger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractTrigger : MonoBehaviour {
+
+    [SerializeField]GameObject objectInfo;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("InteractTrigger init");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TriggerEnter");
+        if (other.gameObject.transform.parent.tag == "Item")
+        {
+            Debug.Log("Item Dectected");
+            objectInfo = other.transform.parent.gameObject;
+            Debug.Log(objectInfo.GetComponent<Item>().id);
+        }
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            objectInfo = other.transform.parent.gameObject;
+            Debug.Log("Wall Dectected");
+        }
+        objectInfo = other.transform.parent.gameObject;
+
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        objectInfo = null;
+    }
+}
