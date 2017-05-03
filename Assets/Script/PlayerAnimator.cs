077b989f54ed2bd998dcee456489e540e77a07e4
kjyong1983 <kjyong1983@gmail.com>
2017-05-03 19:53:41 +0900

    필요없는 주석 제거.

--- Assets/Script/PlayerAnimator.cs
+++ Assets/Script/PlayerAnimator.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour {
 
     Animator anim;
-    PlayerController p;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponentInChildren<Animator>();
-        p = GetComponent<PlayerController>();
 	}
 	
 	// Update is called once per frame
@@ -20,23 +18,18 @@ public class PlayerAnimator : MonoBehaviour {
 
     public void SetMove(Vector2 input)
     {
-        //anim.SetFloat("MoveX", p.Input.x);
-        //anim.SetFloat("MoveY", p.Input.y);
         anim.SetFloat("MoveX", input.x);
         anim.SetFloat("MoveY", input.y);
     }
 
     public void SetLastMove(Vector2 input)
     {
-        //anim.SetFloat("LastMoveX", p.LastInput.x);
-        //anim.SetFloat("LastMoveY", p.LastInput.y);
         anim.SetFloat("LastMoveX", input.x);
         anim.SetFloat("LastMoveY", input.y);
     }
 
     public void SetIsMoving(bool val)
     {
-        //anim.SetBool("IsMoving", p.IsMoving);
         anim.SetBool("IsMoving", val);
     }
 }
