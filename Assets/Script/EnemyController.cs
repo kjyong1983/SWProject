bdc1ab76558a916bab8e499310f77416be5f507a
kjyong1983 <kjyong1983@gmail.com>
2017-06-14 15:40:24 +0900

    5-6층에 몹 추가 배치함..

--- Assets/Script/EnemyController.cs
+++ Assets/Script/EnemyController.cs
@@ -40,52 +40,6 @@ public class EnemyController : MonoBehaviour {
     
     }
 
-
-    //protected void Move(int xDir, int yDir)
-    //{
-    //    Vector2 start = transform.position;
-    //    Vector2 end = start + new Vector2(xDir, yDir);
-    //    StartCoroutine(SmoothMovement(transform, new Vector2(dirX, dirY)));
-    //    //StartCoroutine(SmoothMovement(p.transform.position));
-        
-    //}
-
-    //IEnumerator SmoothMovement(Transform entity, Vector2 input)
-    //{
-
-    //    float t;
-    //    isMoving = true;
-    //    startPos = entity.position;
-    //    t = 0f;
-    //    const float interval = 1f;
-    //    endPos = new Vector3(startPos.x + System.Math.Sign(input.x), startPos.y + System.Math.Sign(input.y), startPos.z);
-
-    //    while (t < interval)
-    //    {
-    //        t += (Time.deltaTime + speed * speedMod) / 2;
-    //        entity.position = Vector3.Lerp(startPos, endPos, t);
-    //        yield return null;
-    //    }
-    //    isMoving = false;
-    //    yield return 0;
-
-    //}
-
-    //protected IEnumerator SmoothMovement(Vector3 end)
-    //{
-    //    float sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-
-    //    while (sqrRemainingDistance > float.Epsilon)
-    //    {
-    //        //Find a new position proportionally closer to the end, based on the moveTime
-    //        Vector3 newPostion = Vector3.MoveTowards(rb.position, end, speed * Time.deltaTime);
-    //        transform.position = newPostion;
-    //        //rb.MovePosition(newPostion);
-    //        sqrRemainingDistance = (transform.position - end).sqrMagnitude;
-    //        yield return null;
-    //    }
-    //}
-
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
